8eb6a33e146de719c8b2e7bfcb2858f77f96cdc4
tnelson <travis.o.nelson@gmail.com>
2012-09-29 14:04:23 -0600

    helloworld to hello travis

--- www/firstServer.js
+++ www/firstServer.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function (request, response) {
-    response.end("Hello World\n");
+    response.end("Hello TheTrav\n");
 });
 
 server.listen(8000);
